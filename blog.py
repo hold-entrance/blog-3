83b1a78430b02de8980215c84059a8e655621ddd
Holden Ranz <ranz.holden@gmail.com>
2015-04-17 12:40:42 -0400

    updated to include display of posts and add post capabilities

--- blog.py
+++ blog.py
@@ -11,6 +11,7 @@ from flask import url_for
 from flask import g
 from functools import wraps
 import os
+import time
 import sqlite3
 
 # configuration 
@@ -55,7 +56,47 @@ def login():
 @app.route('/main')
 @login_required
 def main():
-    return render_template('main.html')
+# show posts
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * FROM posts')
+    posts = [dict(poster=row[0], post=row[1], datestr=row[2], timestr=row[3]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    poster = request.form['poster']
+    post = request.form['post']
+    # Determine strings for date and time 
+    totime = time.localtime(time.time())
+    datestr = str(totime.tm_mon)+"-"+str(totime.tm_mday)+"-"+str(totime.tm_year)
+    ampm = ""
+    timestr = ""
+    if totime.tm_hour == 0:
+        ampm = "AM"
+        timestr = str(totime.tm_hour+12)+":"+str(totime.tm_min)+" "+ampm
+    elif totime.tm_hour != 0 and totime.tm_hour < 12:
+        ampm = "AM"
+        timestr = str(totime.tm_hour)+":"+str(totime.tm_min)+" "+ampm
+    elif totime.tm_hour == 12:
+        ampm = "PM"
+        timestr = str(totime.tm_hour)+":"+str(totime.tm_min)+" "+ampm
+    elif totime.tm_hour > 12:
+        ampm = "PM"
+        timestr = str(totime.tm_hour-12)+":"+str(totime.tm_min)+" "+ampm
+    # Check to make sure other fields are populated with data
+    if not poster or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('INSERT INTO posts (name, post, date, time) VALUES (?, ?, ?, ?)', [poster, post, datestr, timestr])
+        g.db.commit()
+        g.db.close()
+        flash('Your entry has been successfully posted. Thanks for your contribution, '+poster+'!')
+        return redirect(url_for('main'))
+        
 
 @app.route('/logout')
 def logout():
